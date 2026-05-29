d466105c16bfd761668390bb0bdde03f89eb678b
Feed Updater <actions@github.com>
2026-05-29 19:40:44 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -240,8 +240,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Lite fornøyd med atomvåpenavtale",
-      "date": "2026-05-28T19:00:00+02:00"
+      "title": "Storstreik i staten unngått",
+      "date": "2026-05-29T19:00:00+02:00"
     }
   },
   {
