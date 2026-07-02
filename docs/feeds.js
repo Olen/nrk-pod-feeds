10ca13cfa594ac82b55561b3fcbd1731146921b3
Feed Updater <actions@github.com>
2026-07-02 11:42:59 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -1471,8 +1471,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Ruses av billettsalg.",
-      "date": "2026-07-01T06:00:00+02:00"
+      "title": "Barn + mozzarella = trylletriks",
+      "date": "2026-07-02T06:00:00+02:00"
     }
   },
   {
