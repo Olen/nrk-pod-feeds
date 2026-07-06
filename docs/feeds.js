b53cd82c6da5049e2759008f6f57d5ff367f5645
Feed Updater <actions@github.com>
2026-07-06 19:04:35 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -240,8 +240,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Stor spenning før Nato-toppmøte i Ankara",
-      "date": "2026-07-03T19:00:00+02:00"
+      "title": "VM-skandale",
+      "date": "2026-07-06T19:00:00+02:00"
     }
   },
   {
