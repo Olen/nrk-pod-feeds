f766745fb31658114697f8fcef14656a688ea141
Feed Updater <actions@github.com>
2026-07-15 17:37:24 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -240,8 +240,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Situasjonen tilspisser seg i områdene rundt Hormuz-stredet",
-      "date": "2026-07-14T19:00:00+02:00"
+      "title": "Står gamle varmerekorder for fall?",
+      "date": "2026-07-15T19:00:00+02:00"
     }
   },
   {
