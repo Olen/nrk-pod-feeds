e402351811fec3f4c9d1397880115837c73f8d8f
Feed Updater <actions@github.com>
2026-07-16 18:19:12 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -240,8 +240,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Står gamle varmerekorder for fall?",
-      "date": "2026-07-15T19:00:00+02:00"
+      "title": "Dieseluro i Finnmark",
+      "date": "2026-07-16T19:00:00+02:00"
     }
   },
   {
