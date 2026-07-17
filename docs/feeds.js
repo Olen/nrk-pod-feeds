f9836070f91d423348d64e24b2432cb278a0837b
Feed Updater <actions@github.com>
2026-07-17 19:31:12 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -240,8 +240,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Dieseluro i Finnmark",
-      "date": "2026-07-16T19:00:00+02:00"
+      "title": "Kraftig brann i Drammen",
+      "date": "2026-07-17T19:00:00+02:00"
     }
   },
   {
