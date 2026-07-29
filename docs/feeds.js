2c2c221513b0fc2104e0f58cf8cd80e80f258ff5
Feed Updater <actions@github.com>
2026-07-29 17:36:37 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -240,8 +240,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Zelenskyj og Netanyahu møter Trump",
-      "date": "2026-07-28T19:00:00+02:00"
+      "title": "Kraftige reaksjoner på Fifas investorplaner",
+      "date": "2026-07-29T19:00:00+02:00"
     }
   },
   {
