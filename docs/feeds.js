df14e2c485e7041953d8fe343540cd579f9d0d17
Feed Updater <actions@github.com>
2026-07-30 18:43:57 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -240,8 +240,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Kraftige reaksjoner på Fifas investorplaner",
-      "date": "2026-07-29T19:00:00+02:00"
+      "title": "Nye russiske angrep mot Ukraina",
+      "date": "2026-07-30T19:00:00+02:00"
     }
   },
   {
