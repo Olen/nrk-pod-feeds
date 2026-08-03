6590af91e9efd32fc35007d0841228695d653e88
Feed Updater <actions@github.com>
2026-08-03 19:55:32 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -240,8 +240,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Full splid i verdensfotballen",
-      "date": "2026-07-31T19:00:00+02:00"
+      "title": "EU vil støtte Spania i Ceuta",
+      "date": "2026-08-03T19:00:00+02:00"
     }
   },
   {
