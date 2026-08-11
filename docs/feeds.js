181a9310dabc07a0b6174614b804a5fd880a6c95
Feed Updater <actions@github.com>
2026-08-11 11:04:03 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -361,8 +361,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Keisersnitt gir ikke sykmelding",
-      "date": "2026-08-10T13:30:00+02:00"
+      "title": "Den nyttige skogbrannen",
+      "date": "2026-08-11T13:00:00+02:00"
     }
   },
   {
