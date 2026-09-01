18fba4fdf911f5566e079d68f428984d605979c7
Feed Updater <actions@github.com>
2026-09-01 18:28:26 +0000

    chore(auto): Update podcast feed(s)

--- docs/feeds.js
+++ docs/feeds.js
@@ -238,8 +238,8 @@ const feeds = [
     "season": null,
     "enabled": true,
     "last_episode": {
-      "title": "Hva betyr borgfred?",
-      "date": "2026-08-31T19:00:00+02:00"
+      "title": "Kongen har avlagt ed i Stortinget",
+      "date": "2026-09-01T19:00:00+02:00"
     }
   },
   {
